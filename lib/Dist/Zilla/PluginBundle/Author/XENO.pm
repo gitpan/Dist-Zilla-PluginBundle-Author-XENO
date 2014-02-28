9536c75a7109e7020183c3a7a562f9f8e5bbadfd
Caleb Cushing <xenoterracide@gmail.com>
2014-02-28 22:40:39 +0000

    Import of XENO/Dist-Zilla-PluginBundle-Author-XENO-0.001000 from CPAN.
    gitpan-cpan-distribution: Dist-Zilla-PluginBundle-Author-XENO
    gitpan-cpan-version:      0.001000
    gitpan-cpan-path:         XENO/Dist-Zilla-PluginBundle-Author-XENO-0.001000.tar.gz
    gitpan-cpan-author:       XENO
    gitpan-cpan-maturity:     released

---
+++ lib/Dist/Zilla/PluginBundle/Author/XENO.pm
@@ -0,0 +1,113 @@
+package Dist::Zilla::PluginBundle::Author::XENO;
+use 5.008;
+use strict;
+use warnings;
+
+our $VERSION = '0.001000'; # VERSION
+
+use Moose;
+with qw(
+	Dist::Zilla::Role::PluginBundle::Easy
+);
+
+has install => (
+	isa     => 'Bool',
+	is      => 'ro',
+	lazy    => 1,
+	default => 1,
+);
+
+sub configure {
+	my $self = shift;
+
+	my @plugins = (
+		[ PruneFiles => {
+			filenames => [ qw( dist.ini weaver.ini ) ],
+		}], qw(
+		AutoPrereqs
+		ReadmeFromPod
+		OurPkgVersion
+		PodWeaver
+
+		MetaProvides::Package
+		MetaJSON
+
+		RunExtraTests
+		PodCoverageTests
+		PodSyntaxTests
+		Test::ReportPrereqs
+		Test::Compile
+		Test::EOL
+		Test::Portability
+		Test::Perl::Critic
+
+		ContributorsFromGit
+	));
+
+	if ( $ENV{RELEASE_TESTING} ) {
+		push @plugins, (qw(
+		Test::UnusedVars
+		Test::CPAN::Meta::JSON
+		Test::DistManifest
+		Test::Version
+		Test::CPAN::Changes
+		Test::MinimumVersion
+
+		Git::Remote::Check
+		CheckChangesHasContent
+		));
+
+		push @plugins, (
+			[ 'InstallRelease' => { install_command => "cpanm ." } ],
+		) if $self->install;
+
+# must be last
+		push @plugins, ('Clean'),
+	}
+
+	$self->add_plugins( @plugins );
+	return;
+}
+
+1;
+
+# ABSTRACT: Author Bundle for Caleb Cushing
+
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+Dist::Zilla::PluginBundle::Author::XENO - Author Bundle for Caleb Cushing
+
+=head1 VERSION
+
+version 0.001000
+
+=head1 SYNOPSIS
+
+in C<dist.ini>
+
+	[@Author::XENO]
+	install = 0 ; optional, disables InstallRelease
+
+=head1 METHODS
+
+=head2 configure
+
+=head1 AUTHOR
+
+Caleb Cushing <xenoterracide@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is Copyright (c) 2014 by Caleb Cushing.
+
+This is free software, licensed under:
+
+  The Artistic License 2.0 (GPL Compatible)
+
+=cut
